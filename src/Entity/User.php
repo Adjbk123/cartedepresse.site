18058c119f0720bc605d63eaae1c6486fd61b99b
Adjbk123 <adjibako123@gmail.com>
2025-04-10 11:37:32 +0100

    A4

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['email'], message: 'Un compte existe déjà avec ce mail')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
